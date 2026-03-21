272a5f4f18f1104a01787c9f4ecf4cac576432c1
Julian Suszko <s33103@pjwstk.edu.pl>
2026-03-21 17:27:58 +0100

    Added user class

--- EquipmentRental/Models/User.cs
+++ EquipmentRental/Models/User.cs
@@ -1,6 +1,21 @@
+using EquipmentRental.Enums;
+
 namespace EquipmentRental.Models;
 
 public abstract class User
 {
+    public static int IdCounter = 1;
     
+    public int Id { get; private set; }
+    public string Name { get; private set; }
+    public string Surname { get; private set; }
+    public UserType Type { get; set; }
+
+    protected User(string name, string surname, UserType type)
+    {
+        Id = IdCounter++;
+        Name = name;
+        Surname = surname;
+        Type = type;
+    }
 }
