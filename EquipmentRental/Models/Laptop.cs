1d5b7dec9ad94fedb99e629c9e8e08b9f39d45a4
Julian Suszko <s33103@pjwstk.edu.pl>
2026-03-21 15:52:55 +0100

    Added Laptop class

---
+++ EquipmentRental/Models/Laptop.cs
@@ -0,0 +1,17 @@
+namespace EquipmentRental.Models;
+
+public class Laptop : Equipment
+{
+    public int RamSizeGB { get; set; }
+    public string Processor { get; set; }
+    public string GraphicsCard { get; set; }
+    public string[] Disks { get; set; }
+    
+    public Laptop(string name, int ramSizeGb, string processor, string graphicsCard, string[] disks) : base(name)
+    {
+        RamSizeGB = ramSizeGb;
+        Processor = processor;
+        GraphicsCard = graphicsCard;
+        Disks = disks;
+    }
+}
