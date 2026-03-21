6a6aa5bbd262b379f5f0e79afd84636633193283
Julian Suszko <s33103@pjwstk.edu.pl>
2026-03-21 17:50:48 +0100

    Added rental class

---
+++ EquipmentRental/Models/Rental.cs
@@ -0,0 +1,28 @@
+using EquipmentRental.Enums;
+
+namespace EquipmentRental.Models;
+
+public class Rental
+{
+    public static int IdCounter = 1;
+
+    public int Id { get; private set; }
+
+    public User RentingUser { get; private set; }
+    public Equipment RentedEquipment { get; private set; }
+
+    public DateTime RentDate { get; private set; }
+    public DateTime EndDate { get; private set; }
+    public DateTime? ReturnDate { get; private set; }
+
+    public Rental(User rentingUser, Equipment rentedEquipment, int rentingDays)
+    {
+        Id = IdCounter++;
+        RentingUser = rentingUser;
+        RentedEquipment = rentedEquipment;
+        RentDate = DateTime.Now;
+        EndDate = RentDate.AddDays(rentingDays);
+
+        RentedEquipment.Status = EquipmentStatus.Rented;
+    }
+}
