3fa99ecef9e7ac3f377a9fb6afe3b94b4f4ec46f
Julian Suszko <s33103@pjwstk.edu.pl>
2026-03-21 18:58:22 +0100

    Added RentalService

---
+++ EquipmentRental/Services/RentalService.cs
@@ -0,0 +1,62 @@
+using System.Reflection.PortableExecutable;
+using EquipmentRental.Enums;
+using EquipmentRental.Models;
+
+namespace EquipmentRental.Services;
+
+public class RentalService
+{
+    private DataStore  _dataStore;
+    private int penalty = 5;
+    
+    public RentalService(DataStore dataStore)
+    {
+        _dataStore = dataStore;
+    }
+
+    public Rental RentEquipment(User user, Equipment equipment, int days)
+    {
+        if (equipment.Status != EquipmentStatus.Available)
+        {
+            throw new InvalidOperationException($"{equipment.Name} is not available.");
+        }
+
+        int rentalsCount = 0;
+        int maxRentalsCount = user.MaxRentals;
+        
+        foreach (Rental rental in _dataStore.Rentals)
+        {
+            if (rental.RentingUser == user && rental.ReturnDate == null)
+            {
+                rentalsCount++;
+            }
+        }
+
+        if (rentalsCount >= maxRentalsCount)
+        {
+            throw new InvalidOperationException($"{user.Name} cannot have more than {maxRentalsCount} rentals.");
+        }
+        
+        var newRental = new Rental(user, equipment, days);
+        _dataStore.Rentals.Add(newRental);
+        return newRental;
+    }
+
+    public int ReturnEquipment(Rental rental, DateTime returnDate)
+    {
+        if (rental.ReturnDate != null)
+        {
+            throw new InvalidOperationException("This rental is already returned");
+        }
+        
+        rental.EndRental(returnDate);
+
+        if (rental.IsReturnLate())
+        {
+            int delay = (returnDate - rental.EndDate).Days;
+            return delay * penalty;
+        }
+
+        return 0;
+    }
+}
