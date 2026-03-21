823f96895d520540bcd6d8dbab68669207aa9313
Julian Suszko <s33103@pjwstk.edu.pl>
2026-03-21 18:40:10 +0100

    Added DataStore class

---
+++ EquipmentRental/Services/DataStore.cs
@@ -0,0 +1,24 @@
+using EquipmentRental.Models;
+
+namespace EquipmentRental.Services;
+
+public class DataStore
+{
+    public List<Equipment> EquipmentList  { get; }
+    public List<User> Users { get; }
+    public List<Rental> Rentals { get; }
+    
+    public void AddEquipment(Equipment equipment)
+    {
+        EquipmentList.Add(equipment);
+    }
+    public void AddUser(User user)
+    {
+        Users.Add(user);
+    }
+
+    public void AddRental(Rental rental)
+    {
+        Rentals.Add(rental);
+    }
+}
