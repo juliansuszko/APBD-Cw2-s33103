6eab6d55a0e503750fd184b7ea434605a6a4bfe5
Julian Suszko <s33103@pjwstk.edu.pl>
2026-03-24 16:19:03 +0100

    Added imports

--- EquipmentRental/Services/DataStore.cs
+++ EquipmentRental/Services/DataStore.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using EquipmentRental.Models;
 
 namespace EquipmentRental.Services;
--- EquipmentRental/Services/DisplayManager.cs
+++ EquipmentRental/Services/DisplayManager.cs
@@ -1,3 +1,4 @@
+using System;
 using EquipmentRental.Enums;
 using EquipmentRental.Models;
 
